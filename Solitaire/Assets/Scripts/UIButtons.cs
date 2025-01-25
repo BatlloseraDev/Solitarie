30b7d56d15f4bea4f0dc8d0d822bd1166f71d72d
BatlloseraDev <batlloseradeveloper@gmail.com>
2025-01-25 12:53:19 +0100

    Solitarie
    El juego de solitario que hice para aprender mejor objetos por mi cuenta.

---
+++ Solitaire/Assets/Scripts/UIButtons.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButtons : MonoBehaviour
+{
+    public GameObject highScorePanel;
+    public GameObject OptionsPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    public void PlayAgain()
+    {
+        highScorePanel.SetActive(false);
+        ResetScene();
+    }
+
+    public void Options()
+    {
+        OptionsPanel.SetActive(true);
+
+    }
+    public void GameModeEasy()
+    {
+        Solitaire game = FindObjectOfType<Solitaire>();
+        game.GameMode = 1;
+        OptionsPanel.SetActive(false);
+        ResetScene();
+
+
+    }
+    public void GameModeHard()
+    {
+        Solitaire game = FindObjectOfType<Solitaire>();
+        game.GameMode = 3;
+        OptionsPanel.SetActive(false);
+        ResetScene();
+
+
+    }
+
+
+    public void ResetScene()
+    {
+
+        UpdateSprite[] cards = FindObjectsOfType<UpdateSprite>();
+        foreach(UpdateSprite card in cards)
+        {
+            if (card.name != "Card")
+            {
+                Destroy(card.gameObject);
+            }
+            
+        }
+        ClearTopValues();
+
+        FindObjectOfType<Solitaire>().PlayCards();
+
+    }
+
+    void ClearTopValues()
+    {
+        Selectable[] selectables = FindObjectsOfType<Selectable>();
+        foreach(Selectable selectable in selectables)
+        {
+            if (selectable.CompareTag("Top"))
+            {
+                selectable.suit = null;
+                selectable.value = 0;
+            }
+        }
+    }
+}
---
+++ Solitaire/Assets/Scripts/UpdateSprite.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateSprite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite cardFace;
+    public Sprite cardBack;
+    private SpriteRenderer spriteRenderer;
+    private Selectable selectable;
+    private Solitaire solitaire;
+    private UserInput userInput; 
+
+
+
+    void Start()
+    {
+        List<string> deck = Solitaire.GenerateDeck();
+        solitaire = FindObjectOfType<Solitaire>();
+        userInput = FindObjectOfType<UserInput>();
+        int i = 0;
+
+        foreach(string card in deck)
+        {
+            if(this.name == card)
+            {
+                cardFace = solitaire.cardFaces[i];
+       
+                break;
+            }
+            i++;
+        }
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        selectable = GetComponent<Selectable>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(selectable.faceUp == true)
+        {
+            spriteRenderer.sprite = cardFace;
+          //  GetComponentInChildren<GameObject>().SetActive(true);
+        }
+        else
+        {
+            spriteRenderer.sprite = cardBack;
+            //GetComponentInChildren<GameObject>().SetActive(false);
+        }// cambio de caras cuando estan ocultas (cambiar esto para que haya una ligera animacion) 
+
+        if (userInput.slot1)
+        {
+
+            if (name == userInput.slot1.name)
+            {
+                spriteRenderer.color = Color.yellow;
+            }
+            else
+            {
+                spriteRenderer.color = Color.white;
+            }
+        }//cuando la tenga seleccionada cambiara el color
+
+
+
+
+
+
+    }
+}
